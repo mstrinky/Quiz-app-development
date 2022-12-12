41908a6318d521fa6da0adb2f2e5eefde5a45254
Mst Rinky Akter <mstrinkya@gmail.com>
2022-12-12 23:58:39 +0600

    questions

---
+++ question.js
@@ -0,0 +1,25 @@
+let question = [
+    {
+        num:1,
+        question:'What does HTML stand for',
+        answer:'Hyper text markup language',
+        option:[
+            "Hyper text preprocessor",
+            "Hyper text markup language",
+            "Hyper text multiple language",
+            "Hyper tool multi language"
+        ]
+    },
+    {
+        num:2,
+        question:'What does CSS stand for',
+        answer:'Cascading style sheet',
+        option:[
+            "Common style sheet",
+            "Colorful style sheet",
+            "Computer style sheet",
+            "Cascading style sheet"
+        ]
+    },
+
+]
