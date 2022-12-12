fff3b85431affca2225f5e46d6c1cb467c988b01
Mst Rinky Akter <mstrinkya@gmail.com>
2022-12-13 00:26:12 +0600

    problem

--- question.js
+++ question.js
@@ -21,5 +21,38 @@ let question = [
             "Cascading style sheet"
         ]
     },
+    {
+        num:3,
+        question:'What does PHP stand for',
+        answer:'Hypertext preprocessor',
+        option:[
+            "Hypertext preprocessor",
+            "Hypertext programming",
+            "Hypertext preprogramming",
+            "Hometext preprocessor"
+        ]
+    },
+    {
+        num:4,
+        question:'What does SQL stand for',
+        answer:'Structured query language',
+        option:[
+            "Stylish question language",
+            "Stylesheet query language",
+            "Statement question language",
+            "Structured query language"
+        ]
+    },
+    {
+        num:5,
+        question:'What does XHL stand for',
+        answer:'eXtensible markup language',
+        option:[
+            " eXtensible markup language",
+            "eXecutable multiple language",
+            "eXtra multi-program language",
+            "eXamina multiple  language"
+        ]
+    },
 
 ]
